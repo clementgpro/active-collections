75778ba3e66bc7fc65bb7d926fed5c0bc50e347d
Benjamin <Benjamin@Benjamin-PC>
2014-10-17 13:38:58 +0200

    lol

--- active-collections/src/Set.java
+++ active-collections/src/Set.java
@@ -1,4 +1,15 @@
+import java.util.List;
+
 
 public class Set implements IUniqueness {
-
+	
+	// Content of this collection
+	private List<Object> content;
+	
+	// Collection linked
+	private Set link;
+	
+	public Set(List<Object> content) {
+		this.content = content;
+	}
 }
