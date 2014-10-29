578b948e070da8c154111b9fde37e887ba144385
Benjamin <Benjamin@Benjamin-PC>
2014-10-29 14:27:17 +0100

    merge

--- active-collections/src/observer/impl/AbstractCollection.java
+++ active-collections/src/observer/impl/AbstractCollection.java
@@ -15,38 +15,13 @@ public abstract class AbstractCollection<T> extends Subject<T> implements IColle
 	public AbstractCollection(List<T> content) {
 		this.content = content;
 	}
-	
+
 	public AbstractCollection() {
 		this.content = new ArrayList<T>();
 	}
 
 	public List<T> getContent() {
 		return content;
-	}
-
-	/**
-	 * Intersection of the content of the Collection (List<T>)
-	 * 
-	 * @param b
-	 * @return content intersected
-	 */
-	protected List<T> contentIntersection(List<T> b) {
-		// on cree C
-		List<T> newC = new ArrayList<T>();
-
-		// on ajoute
-		List<T> contentOfB = b;
-		int sizeOfB = contentOfB.size();
-		T tmpContent = null;
-		for (int i = 0; i < sizeOfB; i++) {
-			tmpContent = contentOfB.get(i);
-			if (!content.contains(tmpContent)) {
-				add(newC, tmpContent);
-			}
-		}
-
-		return newC;
-		
 	}
 	
 	/**
@@ -75,24 +50,6 @@ public abstract class AbstractCollection<T> extends Subject<T> implements IColle
 		link(c, b);
 
 		return c;
-	}
-
-	/**
-	 * Union of the content of the Collection (List<T>)
-	 * @param b
-	 * @return
-	 */
-	protected List<T> contentUnion(List<T> b) {
-		// on cree C
-		List<T> newC = new ArrayList<T>(content);
-
-		// evite les doublons
-		for (int i = 0; i < b.size(); i++) {
-			System.out.println("-" + b.get(i) + "-");
-			add(newC, b.get(i));
-		}
-
-		return newC;
 	}
 	
 	/**
