73f838bc7a10be11c2602406c0019ce9c7a15c42
Benjamin <Benjamin@Benjamin-PC>
2014-10-30 14:44:49 +0100

    Uncoment OrderedSet + Sequence

--- active-collections/src/observer/impl/OrderedSet.java
+++ active-collections/src/observer/impl/OrderedSet.java
@@ -1,28 +1,26 @@
 package observer.impl;
-//public class OrderedSet {
-//	private Set set;
-//	private Sequence s;
-//
-//	public OrderedSet(Set set, Sequence s) {
-//		super();
-//		this.set = set;
-//		this.s = s;
-//	}
-//
-//	public Set getSet() {
-//		return set;
-//	}
-//
-//	public void setSet(Set set) {
-//		this.set = set;
-//	}
-//
-//	public Sequence getS() {
-//		return s;
-//	}
-//
-//	public void setS(Sequence s) {
-//		this.s = s;
-//	}
-//
-//}
+
+import java.util.List;
+
+public class OrderedSet<T> extends AbstractCollection<T> {
+	
+	private Set<T> set;
+	private Sequence<T> sequuence;
+
+	public OrderedSet(List<T> content) {
+		super(content);
+	}
+
+	@Override
+	public void add(T element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void add(List<T> newList, T element) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- active-collections/src/observer/impl/Sequence.java
+++ active-collections/src/observer/impl/Sequence.java
@@ -1,5 +1,25 @@
 package observer.impl;
 
-//public class Sequence implements IOrdered {
-//
-//}
+import java.util.List;
+
+import observer.interfaces.IOrdered;
+
+public class Sequence<T> extends AbstractCollection<T> implements IOrdered<T> {
+	
+	public Sequence(List<T> content) {
+		super(content);
+	}
+
+	@Override
+	public void add(T element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void add(List<T> newList, T element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
